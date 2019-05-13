17a60547b2d5e80d06fc8a99311b879587e4d5c2
Marcela Pagoada <mpagoada@westinghousehs.org>
2019-05-13 22:21:50 +0000

    HTML Updates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // JavaScript File
+/*global $*/
+
+/* $(".enter").click(function(){
+    $.ajax({
+        url: "https://samples.openweathermap.org/data/2.5/forecast?zip=94040&appid=b6907d289e10d714a6e88b30761fae22",
+        method: "GET",
+        success: function(response) {
+            $("#id").html("<img src='" + response.list.weather.description + "'/>");
+        }
+    });
+}); */
+
+var zipcodeInput = $("input").val();
+
+$(".enter").click(function(){
+    $("#zipcode").html(zipcodeInput);
+});
