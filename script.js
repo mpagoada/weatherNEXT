912ca63d7ededb72622e8331cc90dc25e5a9dfc1
Marcela Pagoada <mpagoada@westinghousehs.org>
2019-06-10 21:39:49 +0000

    In progress

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // JavaScript File
 /*global $*/
 
+
 $(".enter").click(function(){
-   var zipcodeInput = $('input').val();
-   $('#zipcode').html(zipcodeInput);
-   var zipcodeUrl = 'https://samples.openweathermap.org/data/2.5/forecast?zip=' + zipcodeInput + '&appid=b6907d289e10d714a6e88b30761fae22';
+    console.log('in herr');
+  var zipcodeInput = $('input').val();
+  $('#zipcode').html(zipcodeInput);
+  var zipcodeUrl = 'https://samples.openweathermap.org/data/2.5/forecast?zip=' + zipcodeInput + '&appid=b6907d289e10d714a6e88b30761fae22';
   
    
    
@@ -14,11 +16,9 @@ $(".enter").click(function(){
         success: function(response) {
             var minTemp = response.data.list[0].temp_min;
             var maxTemp = response.data[0].temp_max;
-            var skyLook = response.data[1].description;
             
-            $('#mintemp').html('minTemp');
-            $('#maxtemp').html('maxTemp');
-            $('#skylook').html('skyLook');
+            $('#mintemp').html(minTemp);
+            $('#maxtemp').html(maxTemp);
         }
     });
 });
